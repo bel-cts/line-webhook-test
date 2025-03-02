2eb19b742a921182d3d88bbdc62806c704414db6
bel-cts <58722471+bel-cts@users.noreply.github.com>
2025-03-02 16:49:51 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 file_put_contents('webhook_log.txt', file_get_contents('php://input') . "\n", FILE_APPEND);
 http_response_code(200);
 echo "OK";
+file_put_contents('log.txt', $input . PHP_EOL, FILE_APPEND);  // Log เนื้อหาเต็มจาก LINE
+
 ?>
